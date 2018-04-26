5f9e02c83f4d0ff93b73d0cf49d5496c8f8830a1
Emano Waldeck <emano.waldeck@gmail.com>
2018-04-26 12:16:18 +0430

    version 0.1.1

--- background.js
+++ background.js
@@ -1,11 +1,20 @@
 /* globals webext */
 'use strict';
 
+var isFirefox = navigator.userAgent.indexOf('Firefox') !== -1;
+
 var offline = {
   observe: ({frameId, url}) => {
     if (frameId === 0) {
+      const redirectUrl = chrome.runtime.getURL('/data/offline/index.html?rd=' + encodeURIComponent(url));
+
+      if (isFirefox) {
+        window.setTimeout(() => chrome.tabs.update({
+          url: redirectUrl
+        }), 0);
+      }
       return {
-        redirectUrl: chrome.runtime.getURL('/data/offline/index.html?rd=' + encodeURIComponent(url))
+        redirectUrl
       };
     }
     else {
@@ -47,4 +56,39 @@ webext.browserAction.on('clicked', async() => {
     enabled: false
   })).enabled === false;
   webext.storage.set({enabled});
+});
+
+// FAQs and Feedback
+webext.runtime.on('start-up', () => {
+  const {name, version, homepage_url} = webext.runtime.getManifest(); // eslint-disable-line camelcase
+  const page = homepage_url; // eslint-disable-line camelcase
+  // FAQs
+  webext.storage.get({
+    'version': null,
+    'faqs': true,
+    'last-update': 0,
+  }).then(prefs => {
+    if (prefs.version ? (prefs.faqs && prefs.version !== version) : true) {
+      const now = Date.now();
+      const doUpdate = (now - prefs['last-update']) / 1000 / 60 / 60 / 24 > 30;
+      webext.storage.set({
+        version,
+        'last-update': doUpdate ? Date.now() : prefs['last-update']
+      }).then(() => {
+        // do not display the FAQs page if last-update occurred less than 30 days ago.
+        if (doUpdate) {
+          const p = Boolean(prefs.version);
+          webext.tabs.create({
+            url: page + '?version=' + version +
+              '&type=' + (p ? ('upgrade&p=' + prefs.version) : 'install'),
+            active: p === false
+          });
+        }
+      });
+    }
+  });
+  // Feedback
+  webext.runtime.setUninstallURL(
+    page + '?rd=feedback&name=' + name + '&version=' + version
+  );
 });
