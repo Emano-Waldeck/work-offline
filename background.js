ac1985cfe38f6b699080608531ff69333b5ab409
Emano Waldeck <emano.waldeck@gmail.com>
2018-07-26 19:18:24 +0430

    fixes #1

--- background.js
+++ background.js
@@ -2,11 +2,27 @@
 'use strict';
 
 var isFirefox = navigator.userAgent.indexOf('Firefox') !== -1;
+var mode = 'per-tab'; // global or per-tab
+var tabs = {}; // index of offline tabs
+
+chrome.runtime.onMessage.addListener((request, sender, response) => {
+  if (request.method === 'remove.tab') {
+    delete tabs[sender.tab.id];
+    reset().then(response);
+    return true;
+  }
+});
 
 var offline = {
-  observe: ({frameId, url}) => {
+  observe: ({frameId, tabId, url}) => {
+    // per-tab mode
+    if (mode === 'per-tab' && tabs[tabId] !== true) {
+      return;
+    }
     if (frameId === 0) {
-      const redirectUrl = chrome.runtime.getURL('/data/offline/index.html?rd=' + encodeURIComponent(url));
+      const redirectUrl = chrome.runtime.getURL(
+        `/data/offline/index.html?id=${tabId}&rd=${encodeURIComponent(url)}&mode=${tabs[tabId] ? 'per-tab' : mode}`
+      );
 
       if (isFirefox) {
         window.setTimeout(() => chrome.tabs.update({
@@ -24,32 +40,49 @@ var offline = {
     }
   }
 };
-offline.on = () => chrome.webRequest.onBeforeRequest.addListener(offline.observe, {
+offline.install = () => chrome.webRequest.onBeforeRequest.addListener(offline.observe, {
   urls: ['*://*/*']
 }, ['blocking']);
-offline.off = () => chrome.webRequest.onBeforeRequest.removeListener(offline.observe);
-
-offline.on();
-
-var reset = enabled => {
-  offline[enabled ? 'on' : 'off']();
-
-  webext.browserAction.setIcon({
-    path: {
-      '16': 'data/icons' + (enabled ? '' : '/disabled') + '/16.png',
-      '32': 'data/icons' + (enabled ? '' : '/disabled') + '/32.png',
-      '64': 'data/icons' + (enabled ? '' : '/disabled') + '/64.png'
-    }
-  });
-  webext.browserAction.setTitle({
-    title: 'Your browser is in ' + (enabled ? 'offline' : 'online') + ' mode'
-  });
+offline.on = (m = 'global') => {
+  mode = m;
+  offline.install();
+};
+offline.off = () => {
+  mode = 'per-tab';
+  // only disable the observer if there is no tab left in the tabs object
+  if (Object.keys(tabs).length === 0) {
+    chrome.webRequest.onBeforeRequest.removeListener(offline.observe);
+  }
+  else {
+    offline.install();
+  }
 };
 
+var path = (enabled, extra = {}) => Object.assign({
+  path: {
+    '16': 'data/icons' + (enabled ? '' : '/disabled') + '/16.png',
+    '32': 'data/icons' + (enabled ? '' : '/disabled') + '/32.png',
+    '64': 'data/icons' + (enabled ? '' : '/disabled') + '/64.png'
+  }
+}, extra);
+
+var reset = async enabled => {
+  if (enabled === undefined) {
+    enabled = (await webext.storage.get({
+      enabled: false
+    })).enabled;
+  }
+  offline[enabled ? 'on' : 'off']();
+
+  webext.browserAction.setIcon(path(enabled));
+  const title = enabled ? 'Your browser is in offline mode' : `Your browser is in online mode
+  
+ Use page's context menu to toggle offline mode for one or more tabs`;
+  webext.browserAction.setTitle({title});
+};
+// start up
+reset();
 webext.storage.on('changed', ps => reset(ps.enabled.newValue)).if(ps => ps.enabled);
-webext.storage.get({
-  enabled: false
-}).then(ps => reset(ps.enabled));
 
 webext.browserAction.on('clicked', async() => {
   const enabled = (await webext.storage.get({
@@ -57,6 +90,46 @@ webext.browserAction.on('clicked', async() => {
   })).enabled === false;
   webext.storage.set({enabled});
 });
+
+// Page context menu
+chrome.contextMenus.create({
+  id: 'toggle.offline',
+  title: 'Toggle offline mode for this tab',
+  contexts: ['PAGE', 'TABS'].map(key => chrome.contextMenus.ContextType[key]).filter(s => s)
+});
+webext.contextMenus.on('clicked', async(info, tab) => {
+  if (tabs[tab.id]) {
+    delete tabs[tab.id];
+  }
+  else {
+    tabs[tab.id] = true;
+  }
+  webext.browserAction.setIcon(path(tabs[tab.id], {
+    tabId: tab.id
+  }));
+  reset();
+}).if(info => info.menuItemId === 'toggle.offline');
+
+// change toolbar icon if tab gets refreshed in per-tab mode
+webext.tabs.on('updated', tabId => webext.browserAction.setIcon(path(true, {
+  tabId
+}))).if(tabId => tabs[tabId]);
+
+// restore
+if (localStorage.length) {
+  const arr = [];
+  let msg = 'The following tabs were on offline mode before the restart:\n\n';
+  for (let i = 0; i < localStorage.length; i += 1) {
+    const key = localStorage.key(i);
+    const url = localStorage.getItem(key);
+    msg += '\n' + url;
+    localStorage.removeItem(key);
+    arr.push(url);
+  }
+  if(window.confirm(msg + '\n\n Press OK to restore them')) {
+    arr.forEach(url => chrome.tabs.create({url}));
+  }
+}
 
 // FAQs and Feedback
 webext.runtime.on('start-up', () => {
@@ -66,7 +139,7 @@ webext.runtime.on('start-up', () => {
   webext.storage.get({
     'version': null,
     'faqs': true,
-    'last-update': 0,
+    'last-update': 0
   }).then(prefs => {
     if (prefs.version ? (prefs.faqs && prefs.version !== version) : true) {
       const now = Date.now();
@@ -88,7 +161,7 @@ webext.runtime.on('start-up', () => {
     }
   });
   // Feedback
-  webext.runtime.setUninstallURL(
+  chrome.runtime.setUninstallURL(
     page + '?rd=feedback&name=' + name + '&version=' + version
   );
 });
--- data/offline/index.js
+++ data/offline/index.js
@@ -1,21 +1,38 @@
 'use strict';
 
-var args = (location.search || '?').substr(1).split('&').reduce((p, c) => {
-  const [key, value] = c.split('=');
-  p[key] = decodeURIComponent(value);
+var args = new URLSearchParams(location.search);
 
-  return p;
-}, {});
+if (args.get('mode') === 'per-tab') {
+  document.title += ' (per-tab mode)';
+}
+
+var release = () => {
+  localStorage.removeItem(args.get('id'));
+  location.replace(args.get('rd'));
+};
 
 document.addEventListener('click', ({target}) => {
   const cmd = target.dataset.cmd;
 
   if (cmd === 'retry') {
-    location.replace(args.rd);
+    release();
   }
   else if (cmd === 'go-online' && window.confirm('Are you sure you want to go online?')) {
     chrome.storage.local.set({
       enabled: false
-    }, () => location.replace(args.rd));
+    }, release);
+  }
+  else if (cmd === 'switch-online' && window.confirm('Are you sure you want to switch to online mode for this tab?')) {
+    chrome.runtime.sendMessage({
+      method: 'remove.tab'
+    }, release);
   }
 });
+
+document.body.dataset.mode = args.get('mode');
+chrome.storage.onChanged.addListener(prefs => {
+  document.body.dataset.mode = prefs.enabled.newValue ? 'global' : args.get('mode');
+});
+
+// save to session restore
+localStorage.setItem(args.get('id'), args.get('rd'));
